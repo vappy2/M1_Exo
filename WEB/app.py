7602379878aeb62906edda29ddfa58c07ef24889
vappy2 <43212809+vappy2@users.noreply.github.com>
2020-11-18 16:17:42 +0100

    TP - Loading du dataset avec des infos 📊

--- WEB/app.py
+++ WEB/app.py
@@ -7,21 +7,52 @@ import seaborn as sns
 
 st.title('Visualisation des données')
 
+#Upload du CSV
 st.subheader('Choissisez un tableau de données')
 
-# method for dataset files selector
-def file_selector(folder_path='./dataset'):
+# Méthode pour le data selector
+def file_selector(folder_path=os.path.dirname(os.path.realpath(__file__))+'/dataset'):
     # list files name on folder_path
     filenames = os.listdir(folder_path)
     # select specific dataset from list
-    selected_filename = st.selectbox('Select a dataset',filenames)
+    selected_filename = st.selectbox('Sélectionner un dataset',filenames)
     # return the path / file
     return os.path.join(folder_path,selected_filename)
 
 # define filename as current file selected
 filename = file_selector()
 # display filename selected
-st.info('Dataset selected {}'.format(filename))
+st.info('Le dataset suivant à été chargé : {}'.format(filename))
 
 # Read Dataset
-df = pd.read_csv(filename)
+data = pd.read_csv(filename)
+
+#Soucis : le .DS Store s'affiche
+
+
+st.subheader('Afficher le dataset')
+
+if st.button("Afficher des lignes du dataset") :
+	n = st.number_input('Choissisez le nombre de ligne à afficher',value=5)
+	st.dataframe(data.head(n))
+
+
+if st.checkbox("Afficher les colonnes") :
+
+	#on affiche le nom des colonnes du dataset
+	#Attention, on n'utilise pas le .info car il est toujours nul
+	st.dataframe(data.columns)
+
+if st.checkbox("Afficher le type des colonnes") :
+	st.dataframe(data.dtypes)
+
+if st.button('Show shape'):
+    st.text('Nombre de lignes')
+    st.write(data.shape[0])
+    st.text('Nombre de colonnes')
+    st.write(data.shape[1])
+
+
+#Parce que c'st rigolo
+if st.button("Oh c'est rigolo"):
+        st.balloons()
