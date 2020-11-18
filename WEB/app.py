736601e1dd640e3406ba0f1a9bec91dce9febab8
vappy2 <43212809+vappy2@users.noreply.github.com>
2020-11-18 15:16:40 +0100

    Ajout des dataset 📊

--- WEB/app.py
+++ WEB/app.py
@@ -6,3 +6,22 @@ import matplotlib
 import seaborn as sns
 
 st.title('Visualisation des données')
+
+st.subheader('Choissisez un tableau de données')
+
+# method for dataset files selector
+def file_selector(folder_path='./dataset'):
+    # list files name on folder_path
+    filenames = os.listdir(folder_path)
+    # select specific dataset from list
+    selected_filename = st.selectbox('Select a dataset',filenames)
+    # return the path / file
+    return os.path.join(folder_path,selected_filename)
+
+# define filename as current file selected
+filename = file_selector()
+# display filename selected
+st.info('Dataset selected {}'.format(filename))
+
+# Read Dataset
+df = pd.read_csv(filename)
