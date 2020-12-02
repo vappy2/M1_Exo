48ab94e42e23ed42b5f51b16f1e6ee2488a195ae
vappy2 <43212809+vappy2@users.noreply.github.com>
2020-12-02 16:26:28 +0100

    TP Django - Nouvelles fonctionnalité
    Affichage des todo ✅
    Créer des nouveau todo ✅
    Link css ✅

--- TP_Django/todo/models.py
+++ TP_Django/todo/models.py
@@ -6,10 +6,11 @@ from django.utils import timezone #Cet import permet d'obtenir la date selon où
 # Create your models here.
 class Task(models.Model) :
     content = models.CharField(max_length = 250)
-    is_done = models.BooleanField(default=0)
-    created_date = models.DateTimeField('Date de création')
+    is_done = models.BooleanField(default = False)
+    created_date = models.DateTimeField('Date de création', auto_now_add=True)
 
     # Pour que la Task soit pluss représentatif dans le shell il est important de def la fonction __str__()
     #Cette fonction est aussi super utile pour la représentation de l'objet dans l'interface admin
     def __str__(self) :
         return self.content
+    
--- TP_Django/todo/views.py
+++ TP_Django/todo/views.py
@@ -7,22 +7,36 @@ from .models import Task
 # Il faut load notre template
 from django.template import loader
 
+#Si on utilise render 
+from django.shortcuts import render, HttpResponseRedirect
+
 def index(request):
 
     #On stocke nos todo dans une variable
-    todolist = Task.objects.order_by('created_date')
+    todolist = Task.objects.order_by('created_date')[:5]
 
-    #Mise la mise en forme
+    # Mise la mise en forme
     #output = '<br/>'.join([t.content for t in todolist])
 
-    #On load le template qu'on veut utiliser
+    # On load le template qu'on veut utiliser
     template =loader.get_template('todo/index.html')
 
-    #On renseigne l'équivalent des variables dans le template
+    # On renseigne l'équivalent des variables dans le template
     # Ici on n'a que la variable todolist qui est notre liste de nos todos déjà créés
     context = {
         'todolist' : todolist,
     }
 
-    #On implémente ensuite la réponse de la page avec le template
+    # On implémente ensuite la réponse de la page avec le template
     return HttpResponse(template.render(context, request))
+
+    #L'autre manière de l'écrire est la suivante
+    #return render(request, 'todo/indext.html', context)
+
+def add(request) :
+    #On créé notre nouveau todo
+    new_todo = Task(content=request.POST['content'])
+    #On le sauvegarde
+    new_todo.save()
+   # On fait la redirection vers l'index 
+    return HttpResponseRedirect('/todo/')
