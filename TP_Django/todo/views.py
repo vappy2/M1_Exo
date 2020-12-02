126b217994ab829d5e5f2363b543b008deb076f6
vappy2 <43212809+vappy2@users.noreply.github.com>
2020-12-02 14:11:47 +0100

    TP Django - Liste des TODO 🙌🏻
    Incroyable j'ai afficher la liste des Todos 🙌🏻

--- TP_Django/todo/views.py
+++ TP_Django/todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the todo index.")
+    todolist = Task.objects.order_by('created_date')
+    output = '<br/>'.join([t.content for t in todolist])
+    return HttpResponse(output)
